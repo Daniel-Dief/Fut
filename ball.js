fe2b202a6f84c27ec198b089e8aeda7b270d1a79
Perera <Perera@gmail>
2023-09-17 15:08:45 -0300

    bola rolando

--- ball.js
+++ ball.js
@@ -4,7 +4,36 @@ ball.anchor.set(0.5);
 ball.y = 300;
 ball.x = 600;
 
-ball.scale.x *= 0.05; 
-ball.scale.y *= 0.05; 
+ball.scale.x *= 0.05;
+ball.scale.y *= 0.05;
 
 app.stage.addChild(ball);
+
+function rolar(direction) {
+    for (let i=0;i<20;i++) {
+        setTimeout(() => {
+            if (direction == 'up') {
+                ball.y -= 10;
+            } else if (direction == 'down') {
+                ball.y += 10;
+            } else if (direction == 'left') {
+                ball.x -= 10;
+            } else if (direction == 'right') {
+                ball.x += 10;
+            } else if (direction == 'up-left') {
+                ball.y -= 10;
+                ball.x -= 10;
+            } else if (direction == 'up-right') {
+                ball.y -= 10;
+                ball.x += 10;
+            } else if (direction == 'down-left') {
+                ball.y += 10;
+                ball.x -= 10;
+            } else if (direction == 'down-right') {
+                ball.y += 10;
+                ball.x += 10;
+            }
+        }, 15*i);
+    }
+    console.log(direction);
+}
--- movePlayer.js
+++ movePlayer.js
@@ -1,7 +1,15 @@
-keyPress = {};
+let keyPress = {};
+let keyLast1;
+let keyLast2;
 
 window.addEventListener("keydown", (key) => {
     keyPress[key.key] = true;
+    if(key.key == "Shift"){
+        chutarP1();
+    }
+    if(key.key == "Control"){
+        chutarP2();
+    }
 })
 
 window.addEventListener("keyup", (key) => {
@@ -43,33 +51,89 @@ function move(){
 
 function conduzirP1(){
     if(keyPress.w){
-       ball.y = player1.y - 10;
+       ball.y = player1.y - 5;
        ball.x = player1.x;
+       keyLast1 = 'up';
     } else if(keyPress.s){
-       ball.y = player1.y + 10;
+       ball.y = player1.y + 5;
        ball.x = player1.x;
+       keyLast1 = 'down';
     } else if(keyPress.a){
-       ball.x = player1.x - 10;
+       ball.x = player1.x - 5;
        ball.y = player1.y;
+       keyLast1 = 'left';
     } else if(keyPress.d){
-       ball.x = player1.x + 10;
+       ball.x = player1.x + 5;
        ball.y = player1.y;
+       keyLast1 = 'right';
     }
 }
 
 function conduzirP2(){
     if(keyPress.ArrowUp){
-       ball.y = player2.y - 10;
+       ball.y = player2.y - 5;
        ball.x = player2.x;
+       keyLast2 = 'up';
     } else if(keyPress.ArrowDown){
-       ball.y = player2.y + 10;
+       ball.y = player2.y + 5;
        ball.x = player2.x;
+       keyLast2 = 'down';
     } else if(keyPress.ArrowLeft){
-       ball.x = player2.x - 10;
+       ball.x = player2.x - 5;
        ball.y = player2.y;
+       keyLast2 = 'left';
     } else if(keyPress.ArrowRight){
-       ball.x = player2.x + 10;
+       ball.x = player2.x + 5;
        ball.y = player2.y;
+       keyLast2 = 'right';
+    }
+}
+
+function chutarP1(){
+    if(posse(player1, ball)){
+        if(keyPress.w && keyPress.d){
+            rolar('up-right');
+        } else if(keyPress.d && keyPress.s){
+            rolar('down-right');
+        } else if(keyPress.s && keyPress.a){
+            rolar('down-left');
+        } else if(keyPress.a && keyPress.w){
+            rolar('up-left');
+        } else if(keyPress.w){
+            rolar('up');
+        } else if(keyPress.s){
+            rolar('down');
+        } else if(keyPress.a){
+            rolar('left');
+        } else if(keyPress.d){   
+            rolar('right');
+        } else {
+            rolar(keyLast1)
+        }
+    }
+}
+
+function chutarP2(){
+    if(posse(player2, ball)){
+        if(keyPress.ArrowUp && keyPress.ArrowRight){
+            rolar('up-right');
+        } else if(keyPress.ArrowRight && keyPress.ArrowDown){
+            rolar('down-right');
+        } else if(keyPress.ArrowDown && keyPress.ArrowLeft){
+            rolar('down-left');
+        } else if(keyPress.ArrowLeft && keyPress.ArrowUp){
+            rolar('up-left');
+        } else if(keyPress.ArrowUp){
+            rolar('up');
+        } else if(keyPress.ArrowDown){
+            rolar('down');
+        } else if(keyPress.ArrowLeft){
+            rolar('left');
+        } else if(keyPress.ArrowRight){   
+            rolar('right');
+        } else {
+            rolar(keyLast2)
+        }
     }
 }
 
